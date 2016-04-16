a76aebf5950dc5dfbd32a9f215a15822fcafc56c
Ire Aderinokun <ire@ireaderinokun.com>
2016-04-16 13:16:34 +0100

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Botkit = require('botkit')
 
 var accessToken = process.env.FACEBOOK_PAGE_ACCESS_TOKEN
 var verifyToken = process.env.FACEBOOK_VERIFY_TOKEN
-var port = process.env.PORT
+var port = process.env.PORT || 3000
 
 if (!accessToken) throw new Error('FACEBOOK_PAGE_ACCESS_TOKEN is required but missing')
 if (!verifyToken) throw new Error('FACEBOOK_VERIFY_TOKEN is required but missing')
